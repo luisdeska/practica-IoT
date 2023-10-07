dd4c496d9c8c35be3330999bbfc3f2e8323f7876
Dayikeynes16 <126136623+Dayikeynes16@users.noreply.github.com>
2023-10-06 19:02:02 -0600

    Update practica git.py

--- practica git.py
+++ practica git.py
@@ -1,7 +1,1 @@
-print("Hola mundo")
 
-print ("Miguel Fernando San Sebastian")
-print("David Jorge Reyes Guzmán")
-print("Ivan Rodriguez Hernandez")
-print("luis ernesto perez montejo")
-print("Rodrigay Alvarez Leon")
