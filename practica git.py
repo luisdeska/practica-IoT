67bfdb05fc1bd9a73ff159bf5c3087b95e9353ab
luisdeska <105251747+luisdeska@users.noreply.github.com>
2023-10-06 19:18:22 -0600

    Update practica git.py

--- practica git.py
+++ practica git.py
@@ -1,17 +1,12 @@
 
-def mostrar_impares(numero):
-    impares = [str(i) for i in range(1, numero + 1) if i % 2 != 0]
-    return ', '.join(impares)
+num= int(input("ingresa un numero: "))
+if num <= 0:
+    print("Ingresa un numero entero positivo")
+else:
+    impares=[]
+    for i in range (num):
+        if (i%2) != 0:
+            impares.append(i)
 
-try:
-    numero = int(input("Por favor, ingresa un número entero positivo: "))
-
-    if numero <= 0:
-        print("Por favor, ingresa un número entero positivo mayor que cero.")
-    else:
-        impares_hasta_numero = mostrar_impares(numero)
-        print("Números impares hasta el número ingresado:", impares_hasta_numero)
-except ValueError:
-    print("Debes ingresar un número entero válido.")
-
+    print(impares)
 
